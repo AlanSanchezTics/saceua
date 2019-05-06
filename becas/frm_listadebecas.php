9ec53589472a6cc29a5d8db707fc20b102f7bec0
LordPalominoGit <hdector1091@gmail.com>
2019-05-06 11:49:09 -0500

    Conexion a servidor saceua

--- becas/frm_listadebecas.php
+++ becas/frm_listadebecas.php
@@ -2,7 +2,7 @@
 echo 'lista de becas';
 
 		
-	$mysqli = new mysqli("localhost", "root", "", "ceuarkos_saceua123");	
+	$mysqli = new mysqli("www.ceuarkos.edu.mx:3306","ceuarkos","C3u4rk@s2018","ceuarkos_saceua_caja");	
 	if (!$mysqli) {
         die("Connection failed: " . mysqli_connect_error());
     }
